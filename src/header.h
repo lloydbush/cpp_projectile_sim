8a4b4257e167bff58b0f3c0ebf9a8cf36b3af8c0
Lloyd Bush <lloydtbush@gmail.com>
2023-10-29 18:50:03 +0100

    29.10.2023 18:50

---
+++ src/header.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <vector>
+
+void printAndWrite(std::vector<double> listT,std::vector<double> listX,std::vector<double> listY,std::vector<double> listVx,std::vector<double> listVy,std::vector<double> listAx,std::vector<double> listAy);
