86dc006378b4a2f6a8541933be742566f6e56337
Lloyd Bush <lloydtbush@gmail.com>
2023-11-04 13:31:28 +0100

    add write cpability and update .gitignore

--- src/header.h
+++ src/header.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <fstream>
 
-void printAndWrite(std::vector<double> listT,std::vector<double> listX,std::vector<double> listY,std::vector<double> listVx,std::vector<double> listVy,std::vector<double> listAx,std::vector<double> listAy,double maxX,double maxY);
+void printAndWrite(std::vector<double> listT,std::vector<double> listX,std::vector<double> listY,std::vector<double> listVx,std::vector<double> listVy,std::vector<double> listAx,std::vector<double> listAy,double maxX,double maxY,std::string file);
 
--- src/write.cpp
+++ src/write.cpp
@@ -1,26 +1,51 @@
 #include "header.h"
 
-void printAndWrite(std::vector<double> listT,std::vector<double> listX,std::vector<double> listY,std::vector<double> listVx,std::vector<double> listVy,std::vector<double> listAx,std::vector<double> listAy,double maxX,double maxY){
+void printAndWrite(std::vector<double> listT,std::vector<double> listX,std::vector<double> listY,std::vector<double> listVx,std::vector<double> listVy,std::vector<double> listAx,std::vector<double> listAy,double maxX,double maxY,std::string file){
+	file+=".csv";
 
-	std::cout<<"t\tx\ty\tvx\tvy\tax\tay\n---\n";
+	std::ofstream writer(file);
+
+	std::string head="t,x,y,vx,vy,ax,ay\n";
+	writer<<head;
+	std::cout<<head;
+
 	for(int i=0;i<listT.size();i++){
 
 		std::cout
 			<<listT.at(i)
-			<<"\t\t"
+			<<","
 			<<listX.at(i)
-			<<"\t\t"
+			<<","
 			<<listX.at(i)
+			<<","
 			<<listY.at(i)
-			<<"\t\t"
+			<<","
 			<<listVx.at(i)
-			<<"\t\t"
+			<<","
 			<<listVy.at(i)
-			<<"\t\t"
+			<<","
 			<<listAx.at(i)
-			<<"\t\t"
+			<<","
 			<<listAy.at(i)
 			<<std::endl;
+
+		writer
+			<<listT.at(i)
+			<<","
+			<<listX.at(i)
+			<<","
+			<<listX.at(i)
+			<<","
+			<<listY.at(i)
+			<<","
+			<<listVx.at(i)
+			<<","
+			<<listVy.at(i)
+			<<","
+			<<listAx.at(i)
+			<<","
+			<<listAy.at(i)
+			<<"\n";
 	}
 	std::cout<<"maxX: "<<maxX<<"\nmaxY: "<<maxY<<std::endl;
 }
