16280d472d03e4f54cf9cb135c2e1a6025330918
Lloyd Bush <lloydtbush@gmail.com>
2023-12-21 10:36:13 +0100

    add plot.py and run.sh

---
+++ plot.py
@@ -0,0 +1,19 @@
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd
+
+###READ CSV###
+fileName=sys.argv[1]+".csv"
+data=pd.read_csv(fileName)
+
+listX=data['x'].tolist()
+listY=data['y'].tolist()
+
+###PLOT###
+
+plt.plot(listX,listY,color='b',label="sim",linestyle='-')
+plt.title("Projectile Flight Path")
+plt.xlabel("x / m")
+plt.ylabel("y / m")
+plt.legend()
+plt.show()
