8b75e3ea69d1a645c9b5495982d2607deaa78264
loraX <142108266+lorax42@users.noreply.github.com>
2024-09-01 12:42:09 +0200

    fix formatting

--- src/header.h
+++ src/header.h
@@ -1,3 +1,5 @@
+// Copyright 2024 lorax
+
 #pragma once
 
 #include <iostream>
@@ -5,18 +7,20 @@
 #include <string>
 #include <fstream>
 
-#include "environment.h"
-#include "projectile.h"
+#include "./environment.h"
+#include "./projectile.h"
 
-//#define DEBUG // uncomment for DEBUG info
+//#define DEBUG  // uncomment for DEBUG info
 
 #ifdef DEBUG
-#define DEBUG_MSSG(mssg) std::cout << "DEBUG: (" << __FILE__ << ":" << __LINE__ << "): " << mssg << "\n";
+#define DEBUG_MSSG(mssg) std::cout \
+    << "DEBUG(" << __FILE__ << ":" << __LINE__ << "): " << mssg << "\n";
 #else
 #define DEBUG_MSSG(mssg)
 #endif
 
-#define ERROR(mssg) std::cout << "ERROR: (" << __FILE__ << ":" << __LINE__ << "): " << mssg << "\n";
+#define ERROR(mssg) std::cout \
+    << "ERROR(" << __FILE__ << ":" << __LINE__ << "): " << mssg << "\n";
 
 void write(
         double t,
